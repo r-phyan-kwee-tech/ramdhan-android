72268b2326875bf5ab70a6617ab9770b0dc1f7a7
တင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2018-05-02 00:15:42 +0800

    state and mock api test complete

--- app/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/di/ViewModelModule.kt
+++ app/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/di/ViewModelModule.kt
@@ -6,5 +6,5 @@ import dagger.Module
 @Suppress("unused")
 @Module
 abstract class ViewModelModule {
-
+    //TODO  Declare View Models According to your needs
 }
---
+++ app/src/test/java/com/marmutech/ramdantimetable/ramadantimetable/api/TimeTableServiceTest.kt
@@ -0,0 +1,85 @@
+package com.marmutech.ramdantimetable.ramadantimetable.api
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import com.marmutech.ramdantimetable.ramadantimetable.model.DayResponse
+import com.marmutech.ramdantimetable.ramadantimetable.util.LiveDataCallAdapterFactory
+import com.marmutech.ramdantimetable.ramadantimetable.util.LiveDataTestUtil.getValue
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.Okio
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.core.IsNull.notNullValue
+import org.junit.After
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@RunWith(JUnit4::class)
+class GithubServiceTest {
+    @Rule
+    @JvmField
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var service: TimeTableDayServie
+
+    private lateinit var mockWebServer: MockWebServer
+
+
+    @Before
+    fun createService() {
+        mockWebServer = MockWebServer()
+        service = Retrofit.Builder()
+                .baseUrl(mockWebServer.url("/"))
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(LiveDataCallAdapterFactory())
+                .build()
+                .create(TimeTableDayServie::class.java)
+    }
+
+    @After
+    fun stopService() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun getTimeTable() {
+        enqueueResponse("time-table-day.json")
+        val yigit = (getValue(service.getTimetable("")) as ApiSuccessResponse).body
+
+        val request = mockWebServer.takeRequest()
+        assertThat(request.path, `is`("/api?query="))
+
+        assertThat<DayResponse>(yigit, notNullValue())
+    }
+
+    @Test
+    fun getTimeTableList() {
+        enqueueResponse("time-table-day-list.json")
+        val yigit = (getValue(service.getTimetableList("")) as ApiSuccessResponse).body
+
+        val request = mockWebServer.takeRequest()
+        assertThat(request.path, `is`("/api?query="))
+
+        assertThat<DayResponse>(yigit, notNullValue())
+    }
+
+
+    private fun enqueueResponse(fileName: String, headers: Map<String, String> = emptyMap()) {
+        val inputStream = javaClass.classLoader
+                .getResourceAsStream("$fileName")
+        val source = Okio.buffer(Okio.source(inputStream))
+        val mockResponse = MockResponse()
+        for ((key, value) in headers) {
+            mockResponse.addHeader(key, value)
+        }
+        mockWebServer.enqueue(
+                mockResponse
+                        .setBody(source.readString(Charsets.UTF_8))
+        )
+    }
+}
