4ceb847142b14eed3f4229fe1af9b39e2376bb91
တင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2018-05-01 15:20:52 +0800

    TimeTable detail complete

--- app/src/test/java/com/marmutech/ramdantimetable/ramadantimetable/repository/TimeTableDayRepositoryTest.kt
+++ app/src/test/java/com/marmutech/ramdantimetable/ramadantimetable/repository/TimeTableDayRepositoryTest.kt
@@ -54,18 +54,52 @@ class TimeTableDayRepositoryTest {
             "  }\n" +
             "}"
 
+    var detailQuery = "{\n" +
+            "  day(dayId: \"${123456}\") {\n" +
+            "    id\n" +
+            "    objectId\n" +
+            "    day\n" +
+            "    dayMm\n" +
+            "    calendarDay\n" +
+            "    hijariDay\n" +
+            "    sehriTime\n" +
+            "    iftariTime\n" +
+            "    sehriTimeDesc\n" +
+            "    iftariTimeDesc\n" +
+            "    sehriTimeDescMmUni\n" +
+            "    sehriTimeDescMmZawgyi\n" +
+            "    iftariTimeDescMmZawgyi\n" +
+            "    iftariTimeDescMmUni\n" +
+            "    isKadir\n" +
+            "    duaAr\n" +
+            "    duaEn\n" +
+            "    duaMmUni\n" +
+            "    duaMmZawgyi\n" +
+            "    countryId\n" +
+            "    stateId\n" +
+            "    createdDate\n" +
+            "    updatedDate\n" +
+            "  }\n" +
+            "}"
+
     @Rule
     @JvmField
     val instantExecutorRule = InstantTaskExecutorRule()
 
     @Test
-    fun loadCountries() {
+    fun loadTimeTableList() {
         repo.loadTimetableDayList("9876543210", 100, 1)
         verify(timetableDao).getDayByStateId("9876543210", 100, offsetManager(100, 1))
     }
 
     @Test
-    fun goToNetwork() {
+    fun loadTimeTable() {
+        repo.loadTimetableDay("123456")
+        verify(timetableDao).getDayById("123456")
+    }
+
+    @Test
+    fun loadTimeTableListFromNetwork() {
 
         val dbData = MutableLiveData<List<TimeTableDay>>()
         `when`(timetableDao!!.getDayByStateId("9876543210", 100, offsetManager(100, 1))).thenReturn(dbData)
@@ -103,6 +137,49 @@ class TimeTableDayRepositoryTest {
         val observer = mock<Observer<Resource<List<TimeTableDay>>>>()
         repo.loadTimetableDayList("9876543210", 100, offsetManager(100, 1)).observeForever(observer)
         verify(timetableDayService, never()).getTimetableList(query)
+        verify(observer).onChanged(Resource.success(timetableDays))
+
+    }
+
+
+    @Test
+    fun loadTimeTableFromNetwork() {
+
+        val dbData = MutableLiveData<TimeTableDay>()
+        `when`(timetableDao!!.getDayById("123456")).thenReturn(dbData)
+        val mockCountry1 = TestUtil.createTimtableDay(1, "123456", "9876543210")
+        val timetableDays: TimeTableDay = mockCountry1
+
+        val call = DayResponse(data = Data(countries = Countries(data = listOf(TestUtil.createCountry("demo"))),
+                days = Days(data = listOf(TestUtil.createTimtableDay(1,"123","123456"))), states = States(listOf(TestUtil.createState("123456"))),
+                country = TestUtil.createCountry("a"),state = TestUtil.createState("1"),
+                day = timetableDays))
+
+        `when`(timetableDayService!!.getTimetable(detailQuery)).thenReturn(ApiUtil.successCall(call))
+        val observer = mock<Observer<Resource<TimeTableDay>>>()
+
+        repo.loadTimetableDay("123456").observeForever(observer)
+        verify(timetableDayService, never()).getTimetableList(detailQuery)
+        val updatedDbData = MutableLiveData<TimeTableDay>()
+        `when`(timetableDao.getDayById("123456")).thenReturn(updatedDbData)
+        dbData.value = null
+        verify(timetableDayService).getTimetable(detailQuery)
+    }
+
+
+    @Test
+    fun goTimetableOffline() {
+        val dbData = MutableLiveData<TimeTableDay>()
+        val mockTimeTable1 = TestUtil.createTimtableDay(1, "123456", "9876543210")
+
+
+        val timetableDays: TimeTableDay = mockTimeTable1
+
+        dbData.value = timetableDays
+        `when`(timetableDao!!.getDayById("123456")).thenReturn(dbData)
+        val observer = mock<Observer<Resource<TimeTableDay>>>()
+        repo.loadTimetableDay("123456").observeForever(observer)
+        verify(timetableDayService, never()).getTimetableList(detailQuery)
         verify(observer).onChanged(Resource.success(timetableDays))
 
     }
