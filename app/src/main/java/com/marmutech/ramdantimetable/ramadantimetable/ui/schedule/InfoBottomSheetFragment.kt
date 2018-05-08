ee78e4b044257123094ed09c6cf1bdee7fda1a23
aungmyolwin <bbannercoe@gmail.com>
2018-05-08 15:29:45 +0630

    prepare from info fragments

--- app/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/ui/schedule/InfoBottomSheetFragment.kt
+++ app/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/ui/schedule/InfoBottomSheetFragment.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import com.marmutech.ramdantimetable.ramadantimetable.R
 import com.marmutech.ramdantimetable.ramadantimetable.di.Injectable
 
-class InfoBottomSheetFragment : BottomSheetDialogFragment(), Injectable {
+class InfoBottomSheetFragment : BottomSheetDialogFragment(), Injectable, View.OnClickListener {
 
     companion object {
         val TAG:String="tag_info_bottom_sheet"
@@ -17,5 +17,15 @@ class InfoBottomSheetFragment : BottomSheetDialogFragment(), Injectable {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         return inflater.inflate(R.layout.fragment_info_bottom_sheet, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- app/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/ui/setting/SettingFragment.kt
@@ -1,17 +1,14 @@
 package com.marmutech.ramdantimetable.ramadantimetable.ui.setting
 
 import android.arch.lifecycle.ViewModelProvider
-import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.marmutech.ramdantimetable.ramadantimetable.R
-import com.marmutech.ramdantimetable.ramadantimetable.R.id.ll_location_layout
 import com.marmutech.ramdantimetable.ramadantimetable.di.Injectable
 import com.marmutech.ramdantimetable.ramadantimetable.util.UserPrefUtil
-import kotlinx.android.synthetic.main.fragment_setting.*
 import javax.inject.Inject
 
 class SettingFragment : Fragment(), Injectable, View.OnClickListener {
@@ -29,10 +26,6 @@ class SettingFragment : Fragment(), Injectable, View.OnClickListener {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        ll_location_layout.setOnClickListener(this)
-
-        val viewModel: SettingViewModel = ViewModelProviders.of(this, viewModelFactory).get(SettingViewModel::class.java)
-        subscribeUi(viewModel)
     }
 
     private fun subscribeUi(viewModel: SettingViewModel) {
@@ -40,8 +33,5 @@ class SettingFragment : Fragment(), Injectable, View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.ll_location_layout -> (activity as SettingActivity).showLocationSelectFrag()
-        }
     }
 }
