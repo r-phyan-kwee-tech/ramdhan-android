9569ffedb37ba69732df23b89828af6cd445d5c2
winhtaikaung <winhtaikaung28@hotmail.com>
2018-05-09 13:20:56 +0800

    license fragment added

--- app/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/di/SplashFragmentBuilderModule.kt
+++ app/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/di/SplashFragmentBuilderModule.kt
@@ -1,9 +1,6 @@
 package com.marmutech.ramdantimetable.ramadantimetable.di
 
-import com.marmutech.ramdantimetable.ramadantimetable.ui.splash.CompleteFragment
-import com.marmutech.ramdantimetable.ramadantimetable.ui.splash.CountryStateSelectionFragment
-import com.marmutech.ramdantimetable.ramadantimetable.ui.splash.FontSelectionFragment
-import com.marmutech.ramdantimetable.ramadantimetable.ui.splash.LandingFragment
+import com.marmutech.ramdantimetable.ramadantimetable.ui.splash.*
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -19,6 +16,5 @@ abstract class SplashFragmentBuilderModule {
     @ContributesAndroidInjector
     abstract fun contributeCountryStateSelectionFragment(): CountryStateSelectionFragment
 
-    @ContributesAndroidInjector
-    abstract fun contributeCompleteFragment(): CompleteFragment
+
 }
