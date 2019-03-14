bc6b99135356acf65b3fd706eb9f44c19f3c7ad5
aungmyolwin <bbannercoe@gmail.com>
2019-03-14 15:41:48 +0630

    landing nav arch component

---
+++ beta/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/beta/DateListFragment.kt
@@ -0,0 +1,72 @@
+package com.marmutech.ramdantimetable.ramadantimetable.beta
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_date_list.view.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [DateListFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [DateListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class DateListFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_date_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.btn_next.setOnClickListener {
+            it.findNavController().navigate(R.id.action_dateListFragment_to_detailFragment)
+        }
+    }
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment DateListFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+                DateListFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
+                }
+    }
+}
--- beta/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/beta/MainActivity.kt
+++ beta/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/beta/MainActivity.kt
@@ -2,15 +2,15 @@ package com.marmutech.ramdantimetable.ramadantimetable.beta
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        /*btn_test.setOnClickListener {
-            throw RuntimeException("it's just test")
-        }*/
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.fragment_host).navigateUp()
     }
 }
