e3676748a84d5c7e753b2eb8176c513f9c21a1f3
amlwin <aungmyo.lwin@foodpanda.com>
2021-05-28 15:40:31 +0800

    remove: toString in POJO

--- app/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/model/Country.kt
+++ app/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/model/Country.kt
@@ -7,16 +7,14 @@ import androidx.room.Index
 
 @Entity(tableName = "country", primaryKeys = ["objectId"], indices = [Index("objectId")])
 data class Country(
-        @ColumnInfo(name = "id")
-        val id: String,
-        @ColumnInfo(name = "objectId")
-        val objectId: String,
-        @ColumnInfo(name = "name")
-        val name: String,
-        @ColumnInfo(name = "createdDate")
-        val createdDate: Int,
-        @ColumnInfo(name = "updatedDate")
-        val updatedDate: Int
-) {
-    override fun toString(): String = name
-}
+    @ColumnInfo(name = "id")
+    val id: String,
+    @ColumnInfo(name = "objectId")
+    val objectId: String,
+    @ColumnInfo(name = "name")
+    val name: String,
+    @ColumnInfo(name = "createdDate")
+    val createdDate: Int,
+    @ColumnInfo(name = "updatedDate")
+    val updatedDate: Int
+)
--- app/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/model/State.kt
+++ app/src/main/java/com/marmutech/ramdantimetable/ramadantimetable/model/State.kt
@@ -6,21 +6,19 @@ import androidx.room.Index
 
 @Entity(tableName = "state", primaryKeys = ["objectId"], indices = [Index("objectId"), Index("countryId")])
 data class State(
-        @ColumnInfo(name = "id")
+    @ColumnInfo(name = "id")
         val id: String,
-        @ColumnInfo(name = "objectId")
+    @ColumnInfo(name = "objectId")
         val objectId: String,
-        @ColumnInfo(name = "nameMmUni")
-        val nameMmUni: String,
-        @ColumnInfo(name = "nameMmZawgyi")
-        val nameMmZawgyi: String,
-        @ColumnInfo(name = "countryId")
-        val countryId: String,
-        @ColumnInfo(name = "createdDate")
-        val createdDate: Int,
-        @ColumnInfo(name = "updatedDate")
-        val updatedDate: Int
+    @ColumnInfo(name = "nameMmUni")
+    val nameMmUni: String,
+    @ColumnInfo(name = "nameMmZawgyi")
+    val nameMmZawgyi: String,
+    @ColumnInfo(name = "countryId")
+    val countryId: String,
+    @ColumnInfo(name = "createdDate")
+    val createdDate: Int,
+    @ColumnInfo(name = "updatedDate")
+    val updatedDate: Int
 
-) {
-    override fun toString(): String = nameMmUni
-}
+)
